d6c9fc8d13ffa0e0f5da13563daedd21306ebaaa
Lucas Silverio <lcs.silva.silverio@gmail.com>
2024-03-12 15:37:31 -0300

    feat: Create Header and insert on screens

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Homepage</router-link> |
-    <router-link to="/about">Sobre o jogo</router-link> |
-    <router-link to="/creator">Sobre o criador</router-link>
-  </nav>
+  <Header />
   <router-view />
 </template>
+
+<script>
+import Header from './components/PageHeader/PageHeader.vue';
+
+export default {
+  components: {
+    Header,
+  }
+};
+</script>
 
 <style>
 #app {
@@ -14,24 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none
-}
-
-nav a.router-link-exact-active {
-  color: black;
-  background-color: #fbbc04;
-  border: 1px solid darkgrey;
-  padding: 10px;
-  border-radius: 8px;
-  cursor: pointer;
 }
 </style>
---
+++ src/components/PageHeader/PageHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="page-header">
+    <h2>{{ pageTitle }}</h2>
+    <nav>
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/about" class="nav-link">Sobre o jogo</router-link>
+      <router-link to="/creator" class="nav-link">Sobre o criador</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pageTitle: 'Catálogo de Skins CS-GO'
+    };
+  }
+};
+</script>
+
+<style scoped>
+.page-header {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-link {
+  color: #fff;
+  text-decoration: none;
+  margin-left: 10px;
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none
+}
+
+nav a.router-link-exact-active {
+  color: black;
+  box-shadow: 0 0 5px #eee;
+  background-image: linear-gradient(to right, #F09819 0%, #EDDE5D 50%, #F09819 100%);
+  border: 1px solid darkgrey;
+  padding: 10px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
