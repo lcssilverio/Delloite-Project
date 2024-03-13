da9da011e1fa1832a1bb28c7711416998544eae3
Lucas Silverio <lcs.silva.silverio@gmail.com>
2024-03-13 14:04:20 -0300

    refactor: Change Favorite button to IconButton component

--- src/components/PostCard/PostCard.vue
+++ src/components/PostCard/PostCard.vue
@@ -2,10 +2,12 @@
   <div class="card">
     <div class="card-header">
       <img :src="item.image" alt="Card Image" class="card-image" />
-      <button v-if="isFavorite()" @click="addFavorite()" class="favorite-button">
-        Favoritar
+      <button v-if="isFavorite()" @click="addFavorite()" class="btn">
+        <IconButton icon="favorito-outlined" bgColor="black"></IconButton>
       </button>
-      <button v-else @click="removeFavorite()" class="unfavorite-button">Favorito</button>
+      <button v-else @click="removeFavorite()" class="btn">
+        <IconButton icon="favorito" bgColor="red"></IconButton>
+      </button>
     </div>
     <div class="card-content">
       <h2>{{ item.name }}</h2>
@@ -15,11 +17,14 @@
 </template>
 
 <script>
+import IconButton from '../IconButton/IconButton.vue';
+
 
 export default {
   props: {
     item: Object,
   },
+  components: { IconButton },
 
   methods: {
     isFavorite() {
@@ -46,16 +51,16 @@ export default {
 
 <style scoped>
 .card {
-  border: 2px solid darkgray;
+  border: 2px solid #a9a9a9;
   border-radius: 10px;
   overflow: hidden;
-  background-color: lightgrey;
+  background-color: #d3d3d3;
 }
 
 .card:hover {
   transform: scale(1.04) perspective(0px);
   box-shadow: 6px 6px 6px rgba(60, 60, 93, 0.33);
-  border: 2px solid white;
+  border: 2px solid #ffffff;
 }
 
 .card-header {
@@ -83,13 +88,13 @@ button {
   cursor: pointer;
 }
 
-.favorite-button {
-  border: 1px solid #000;
-
-}
-
-.unfavorite-button {
-  background-image: linear-gradient(to right, #F09819 0%, #EDDE5D 50%, #F09819 100%);
-  font-weight: bold
+.btn {
+  font-size: 14px;
+  color: #fff;
+  background-color: #d3d3d3;
+  border: 0px;
+  transition: 1000ms;
+  align-items: center;
+  cursor: pointer;
 }
 </style>
